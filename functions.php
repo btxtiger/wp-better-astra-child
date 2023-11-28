398e7023022aaeccdbed97004a6bad8e4b6369d1
Jonas <btxtiger@icloud.com>
2023-11-28 23:49:50 +0100

    Project

--- functions.php
+++ functions.php
@@ -19,7 +19,6 @@ define('CHILD_THEME_ASTRA_CHILD_JONAS_VERSION', '1.0.0');
 function child_enqueue_styles()
 {
     wp_enqueue_style('astra-child-jonas-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_JONAS_VERSION, 'all');
-
 }
 
 add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
